847f21bd3b41361ffc51ab53d670ffb65591b30e
Łukasz Stańczuk <L.stanczuk@gmail.com>
2020-12-03 23:19:02 +0100

    create endpoints

--- src/main/java/com/sda/weather/weather/ForecastController.java
+++ src/main/java/com/sda/weather/weather/ForecastController.java
@@ -1,13 +1,36 @@
 package com.sda.weather.weather;
 
+import com.sda.weather.localization.Localization;
+import com.sda.weather.localization.LocalizationDTO;
+import com.sda.weather.localization.LocalizationFetchService;
+import com.sda.weather.localization.LocalizationMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 class ForecastController {
 
     private final ForecastService forecastService;
+
+
+    @GetMapping("/forecast")
+    ResponseEntity<Forecast> getForecastByCityAndPeriod(@RequestParam("city") String locationName, @RequestParam Integer period) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(forecastService.getForecastByCityAndPeriod(locationName, period));
+    }
+
+    @GetMapping("localization/{id}/forecast")
+    ResponseEntity<ForecastDto> getForecastByIdLocalization(@PathVariable Long id, @RequestParam Integer period, @RequestParam Localization localization) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(forecastService.getForecastByIDLocaliztion(id, period, localization));
+    }
+
 
     // todo let's define endpoints eg.
     //  GET: /localization/{id}/forecast?period=3
--- src/main/java/com/sda/weather/weather/ForecastService.java
+++ src/main/java/com/sda/weather/weather/ForecastService.java
@@ -1,16 +1,26 @@
 package com.sda.weather.weather;
 
+import com.sda.weather.exception.NotFoundException;
+import com.sda.weather.localization.Localization;
+import com.sda.weather.localization.LocalizationDTO;
+import com.sda.weather.localization.LocalizationFetchService;
+import com.sda.weather.localization.LocalizationMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 class ForecastService {
 
     private final ForecastRepository forecastRepository;
+    private final ForecastConnection forecastConnection;
+    private final LocalizationFetchService localizationFetchService;
+    private final LocalizationMapper localizationMapper;
 
-    Forecast createForecast(Forecast forecast) {
-        ForecastConnection forecastConnection = new ForecastConnection();
+
+    Forecast getForecast(Forecast forecast) {
         ForecastDto forecastDto = forecastConnection.getForecast();
         forecast.setAirPressure(forecastDto.getAirPressure());
         forecast.setHumidity(forecastDto.getHumidity());
@@ -19,5 +29,17 @@ class ForecastService {
         forecast.setWindSpeed(forecastDto.getWindSpeed());
 
         return forecastRepository.save(forecast);
+    }
+
+    Forecast getForecastByCityAndPeriod(String locationName, Integer period) {
+        return forecastRepository.findByCityAndPeriod(locationName, period)
+                .orElseThrow(() -> new NotFoundException("Nie można znależć danych"));
+    }
+
+
+    public ForecastDto getForecastByIDLocaliztion(Long id, Integer period, Localization localization) {
+        localization = localizationFetchService.fetchLocalization(id);
+        LocalizationDTO localizationDTO = localizationMapper.mapToLocalizationDto(localization);
+        return null;
     }
 }
