4e0c2e100909db1300de8d41dc4b18691dbdaf66
Łukasz Stańczuk <L.stanczuk@gmail.com>
2020-12-13 15:28:09 +0100

    addintegration with angular

--- src/main/java/com/sda/weather/localization/Localization.java
+++ src/main/java/com/sda/weather/localization/Localization.java
@@ -3,16 +3,18 @@ package com.sda.weather.localization;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.Instant;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Localization {
 
     @Id
@@ -23,4 +25,18 @@ public class Localization {
     String region;
     Double longitude;
     Double latitude;
+
+    public Localization(Long id, String country, String city, String region, Double longitude, Double latitude) {
+        this.id = id;
+        this.country = country;
+        this.city = city;
+        this.region = region;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    @CreatedDate
+    private Instant createdDate;
+    @CreatedBy
+    private String createdBy;
 }
